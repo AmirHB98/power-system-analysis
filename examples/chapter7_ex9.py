e46d4e650e102d9975b83ad5b35e8b7b68009576
AmirHB98 <amirhossein.bagherzadeh@gmail.com>
2025-11-28 23:06:54 +0330

    minor changes in ch7_ex9 and added visualisation

--- examples/chapter7_ex9.py
+++ examples/chapter7_ex9.py
@@ -1,13 +1,16 @@
 # %% imports
-import numpy as np
-import sys
 import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import PowerSystem directly
 from src.power_system import PowerSystem
+from src.power_viz import plot_power_system
 
 # %% Set system parameters
 ps = PowerSystem()
@@ -24,7 +27,7 @@ busdata = [
     [2, 2, 1.045, 0.0, 20, 10, 40, 30, 10, 50, 0],
     [3, 2, 1.03, 0.0, 20, 15, 30, 10, 10, 40, 0],
     [4, 0, 1.00, 0.0, 50, 30, 0, 0, 0, 0, 0],
-    [5, 0, 1.00, 0.0, 60, 40, 0, 0, 0, 0, 0]
+    [5, 0, 1.00, 0.0, 60, 40, 0, 0, 0, 0, 0],
 ]
 
 # Line data
@@ -37,7 +40,7 @@ linedata = [
     [2, 4, 0.06, 0.18, 0.020, 1],
     [2, 5, 0.04, 0.12, 0.015, 1],
     [3, 4, 0.01, 0.03, 0.010, 1],
-    [4, 5, 0.08, 0.24, 0.025, 1]
+    [4, 5, 0.08, 0.24, 0.025, 1],
 ]
 
 # Load the data
@@ -61,3 +64,7 @@ PL = Pgg_array @ (B / ps.basemva) @ Pgg_array + B0 @ Pgg_array + B00 * ps.basemv
 print(f"Total system loss calculated from B coefficients = {PL:.5f} MW")
 
 # %%
+print("Creating visualisation ...")
+plot_power_system(ps, title="5-bus Test system")
+plt.tight_layout()
+plt.show()
